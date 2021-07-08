5214a064a0a1869d72476e0287a0478201cefc92
Patryck Gratão <patryck@polichat.com.br>
2021-07-08 08:52:51 -0300

    :sparkles: Adding css handlers

--- react/components/WishListButton/WishListButton.component.jsx
+++ react/components/WishListButton/WishListButton.component.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
+import { useCssHandles } from "vtex.css-handles";
+import "./WishListButton.styles.css";
 
-const WishListButton = ( { AddListenBtn } ) => {
-  return(
-    <div>
+const CSS_HANDLES = ["wishListButtonContainer", "wishListButtonText"];
+
+const WishListButton = ({ AddListenBtn }) => {
+  const handles = useCssHandles(CSS_HANDLES);
+
+  const handleClick = () => {
+    const wishListButton = document.querySelector(
+      `.${handles.wishListButtonContainer} .vtex-button`
+    );
+
+    wishListButton && wishListButton.click();
+  };
+  return (
+    <div onClick={handleClick} className={`${handles.wishListButtonContainer}`}>
       <AddListenBtn />
-      Add to WishList
+      <div className={`${handles.wishListButtonText}`}>Add to WishList</div>
     </div>
-  )
-}
+  );
+};
 
 export default WishListButton;
